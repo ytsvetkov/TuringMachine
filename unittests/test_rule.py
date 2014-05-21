d51f52f3e5c7fe09a5f440fa56444b9989ca3a56
Yosif Tsvetkov <yosif.tsvetkov.gmail.com>
2014-05-21 15:00:14 +0300

    Initial tests.

---
+++ unittests/test_rule.py
@@ -0,0 +1,9 @@
+import unittest
+from rule import Rule
+
+class RuleTest(unittest.TestCase):
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ unittests/test_tape.py
@@ -0,0 +1,39 @@
+import unittest
+from tape import Tape
+
+class TapeTest(unittest.TestCase):
+    
+    def test_left_right_move(self):
+        test_tape = Tape(['1', '0', '1'], '1', ['0'])
+        result_tape = Tape(['1', '0', '1'], '1', ['0'])
+        result_tape.move_head_left()
+        result_tape.move_head_right()
+        self.assertEqual(test_tape, result_tape)
+
+    def test_right_move(self):
+        test_tape = Tape(['1', '1', '1', '1'],'0', ['0', '1'])
+        result_tape = Tape(['1', '1'],'1', ['1', '0','0', '1'])
+        result_tape.move_head_right()
+        result_tape.move_head_right()
+        self.assertEqual(test_tape, result_tape)
+
+    def test_left_move(self):
+        result_tape = Tape(['1', '1', '1', '1'],'0', ['0', '1'])
+        test_tape = Tape(['1', '1'],'1', ['1', '0','0', '1'])
+        result_tape.move_head_left()
+        result_tape.move_head_left()
+        self.assertEqual(test_tape, result_tape)
+
+    def test_write(self):
+        test_tape = Tape(['1', '1', '1', '1'],'0', ['0', '1'])
+        result_tape = Tape(['1', '1', '1', '1'],'1', ['0', '1'])
+        result_tape.write('0')
+        self.assertEqual(test_tape, result_tape)
+
+    def test_write_error(self):
+        result_tape = Tape(['1', '1', '1', '1'],'0', ['0', '1'])
+        self.assertRaises(TypeError, result_tape.write)
+
+
+if __name__ == '__main__':
+    unittest.main()
