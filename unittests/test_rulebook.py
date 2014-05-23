5aadbc51715cab0c15332afcf7f0a242194b0597
Yosif Tsvetkov <yosif.tsvetkov.gmail.com>
2014-05-23 09:10:03 +0300

    Second test for non-deterministic behaviour.

--- unittests/test_rulebook.py
+++ unittests/test_rulebook.py
@@ -7,8 +7,11 @@ class RulesTest(unittest.TestCase):
     def test_deterministic(self):
         rule1 = rule.Rule(1, 'a', 'a', 1, 'Left')
         rule2 = rule.Rule(1, 'a', 'b', 2, 'Right')
-        rules = rule_book.Rule_Book(rule1, rule2)
-        self.assertFalse(rules.is_deterministic())
+        self.assertRaises(TypeError, rule_book.Rule_Book, rule1, rule2)
+
+        rule3 = rule.Rule(1, 'b', 'a', 1, 'Right')
+        rules = rule_book.Rule_Book(rule1, rule3)
+        self.assertTrue(rules.is_deterministic())
 
 
 if __name__ == '__main__':
