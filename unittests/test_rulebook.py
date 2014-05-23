abc6e0388274c863083c1d64eef225be7ffdde7a
Yosif Tsvetkov <yosif.tsvetkov.gmail.com>
2014-05-23 08:38:36 +0300

    Initial test for determinism

--- unittests/test_rulebook.py
+++ unittests/test_rulebook.py
@@ -1,9 +1,14 @@
 import unittest
-from tape import Tape
-
+import rule
+import rule_book
 
 class RulesTest(unittest.TestCase):
-    pass
+
+    def test_deterministic(self):
+        rule1 = rule.Rule(1, 'a', 'a', 1, 'Left')
+        rule2 = rule.Rule(1, 'a', 'b', 2, 'Right')
+        rules = rule_book.Rule_Book(rule1, rule2)
+        self.assertFalse(rules.is_deterministic())
 
 
 if __name__ == '__main__':
