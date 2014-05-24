f2811cd056b7c0caf61da7a425d3d4f2ec2d0fb6
Yosif Tsvetkov <yosif.tsvetkov.gmail.com>
2014-05-25 00:13:24 +0300

    No stupid file names.

--- machine_builder.py
+++ machine_builder.py
@@ -1,16 +1,16 @@
 import sys
 import re
-import rule
-import tape
+import rule as _rule
+import tape as _tape
 import turing_machine
 
 
-def machine_builder(tapee, statess, accept_statess,
-                    reject_statess, initial_statee, ruless):
+def machine_builder(tape, states, accept_states,
+                    reject_states, initial_state, rules):
     rule_book = []
-    for rulee in ruless:
-        rule_book.append(rule.Rule(*rulee))
-    tapeee = tape.Tape(*tapee)
-    return turing_machine.TuringMachine(tapeee, statess, accept_statess,
-                                        reject_statess, initial_statee,
+    for rule in rules:
+        rule_book.append(_rule.Rule(*rule))
+    tape = _tape.Tape(*tape)
+    return turing_machine.TuringMachine(tape, states, accept_states,
+                                        reject_states, initial_state,
                                         *rule_book)
