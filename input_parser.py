77b666f9a17658b0c3ba3ed440e541e7d14e233e
Yosif Tsvetkov <yosif.tsvetkov.gmail.com>
2014-05-25 00:01:24 +0300

    Separation of differen parse-validations

--- input_parser.py
+++ input_parser.py
@@ -5,15 +5,8 @@ import machine_builder
 
 class SyntacticError(Exception):
 
-    def __init__(self, message, messed_line):
+    def __init__(self, message):
         self.message = message
-        self.messed_line = messed_line
-
-
-machine_rules = []
-states = set()
-accept_states = set()
-reject_states = set()
 
 
 user_tape_regex = r'^\s*\(.*,.,.*\)$'
@@ -22,57 +15,39 @@ user_initial_regex = r'^[0-9]+$'
 user_rule_regex = r'^\([0-9]{1,},.,.,[0-9]{1,},(Left|None|Right)\)$'
 
 
-def parse_validator_from_terminal(ruless, statess, accept_statess,
-                                  reject_statess, initial_statee, tapee):
-    if re.match(user_tape_regex, tapee.strip('\n')) == None:
-        print('error')
+def parse_tape_from_terminal(input_tape):
+    tape = re.match(user_tape_regex, input_tape.strip('\n'))
+    if tape == None:
+        return SyntacticError('There is syntactic error with this tape !')
     else:
-        turing_tape = re.match(user_tape_regex, tapee.strip('\n'))
-        initial_tape = turing_tape.group().strip(')(').split(',')
+        return tape.group().strip(')(').split(',')
 
-    if re.match(user_states_regex, statess.strip('\n')) == None:
-        print('error')
+
+def parse_states_from_terminal(input_states):
+    states = re.match(user_states_regex, input_states.strip('\n'))
+    if states == None:
+        return SyntacticError('There is syntactic error with these states !')
     else:
-        turing_states = re.match(user_states_regex, statess.strip('\n'))
-        for state in turing_states.group().strip('}').split(','):
-            states.add(int(state))
+        machine_states = set()
+        for state in states.group().strip('}').split(','):
+            machine_states.add(int(state))
+        return machine_states
 
-    if accept_statess == '':
-        print('accept: ', accept_states)
-    elif re.match(user_states_regex, accept_statess.strip('\n')) == None:
-        print('error')
+
+def parse_initial_from_terminal(intput_initial_state):
+    initial = re.match(user_initial_regex, intput_initial_state.strip('\n'))
+    if initial == None:
+        return SyntacticError('There is syntactic error with the initial state !')
     else:
-            turing_accept = re.match(
-                user_states_regex, accept_statess.strip('\n'))
-            for state in turing_accept.group().strip('}').split(','):
-                accept_states.add(int(state))
+        return int(initial.group())
 
-    if reject_statess == '':
-        print('reject: ', reject_states)
-    elif re.match(user_states_regex, reject_statess.strip('\n')) == None:
-        print('error')
+
+def parse_rule_from_terminal(input_rule):
+    input_rule = re.match(user_rule_regex, input_rule)
+    if input_rule == None:
+        return SyntacticError('There is syntactic error with this rule !')
     else:
-            turing_accept = re.match(
-                user_states_regex, reject_statess.strip('\n'))
-            for state in turing_accept.group().strip('}').split(','):
-                reject_states.add(int(state))
-
-    if re.match(user_initial_regex, initial_statee.strip('\n')) == None:
-        print('error')
-    else:
-            turing_initial = re.match(
-                user_initial_regex, initial_statee.strip('\n'))
-            initial_state = int(turing_initial.group())
-
-    for rulee in ruless:
-        if re.match(user_rule_regex, rulee) == None:
-            raise SyntacticError(rulee, 434)
-        else:
-            turing_rule = rulee.strip('\n)(').split(',')
-            turing_rule[0] = int(turing_rule[0])
-            turing_rule[3] = int(turing_rule[3])
-            machine_rules.append(turing_rule)
-
-    return machine_builder.machine_builder(initial_tape, states, accept_states,
-                                           reject_states, initial_state,
-                                           machine_rules)
+        rule = input_rule.group().strip('\n)(').split(',')
+        rule[0] = int(rule[0])
+        rule[3] = int(rule[3])
+    return rule
