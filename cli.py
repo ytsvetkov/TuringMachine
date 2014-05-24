14ecf211ed532e4716931dec9f7f300ccce5edc3
Yosif Tsvetkov <yosif.tsvetkov.gmail.com>
2014-05-25 00:05:02 +0300

    More user-frinedly interface but realised poorly

--- cli.py
+++ cli.py
@@ -4,6 +4,7 @@ import re
 import file_parser
 import input_parser
 from turing_machine import *
+import machine_builder
 
 
 accept_invite_message = 'If you need another accept state, \
@@ -22,20 +23,45 @@ rules = []
 
 def main_cli_action():
 
-    tape = input('Initialise the tape:\n')
-    states = input('Enter the states:\n')
-    accept_states = input('Enter the accept states\n')
-    reject_states = input('Enter the reject states\n')
-    initial_state = input('Enter the initial state\n')
-    print(('Enter a rule or press Ctrl+D for end of rules'))
+    while True:
+        tape = input('Initialise the tape:\n')
+        tape = input_parser.parse_tape_from_terminal(tape)
+        if isinstance(tape, input_parser.SyntacticError): print('error')
+        else: break
+
+    while True:
+        states = input('Enter the states:\n')
+        states = input_parser.parse_states_from_terminal(states)
+        if isinstance(states, input_parser.SyntacticError): print('error')
+        else: break
+        
+    while True:
+        accept_states = input('Enter the accept states\n')
+        accept_states = input_parser.parse_states_from_terminal(accept_states)
+        if isinstance(accept_states, input_parser.SyntacticError): print('error')
+        else: break
+
+    while True:
+        reject_states = input('Enter the reject states\n')
+        reject_states = input_parser.parse_states_from_terminal(reject_states)
+        if isinstance(reject_states, input_parser.SyntacticError): print('error')
+        else: break
+
+    while True:
+        initial_state = input('Enter the initial state\n')
+        initial_state = input_parser.parse_initial_from_terminal(initial_state)
+        if isinstance(initial_state, input_parser.SyntacticError): print('error')
+        else: break
+
     try:
         while True:
-            rule = input()
-            rules.append(rule)
+            rule = input('Enter a rule\n')
+            rule = input_parser.parse_rule_from_terminal(rule)
+            if isinstance(rule, input_parser.SyntacticError): print('error')
+            else: rules.append(rule)
     except EOFError:
-        return input_parser.parse_validator_from_terminal(
-            rules, states, accept_states,
-            reject_states, initial_state, tape)
+        return machine_builder.machine_builder(tape, states, accept_states,
+                    reject_states, initial_state, rules)
 
 
 try:
