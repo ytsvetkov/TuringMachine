2262fbaaeb75c806045f962044e5f56d7af3767b
Yosif Tsvetkov <yosif.tsvetkov.gmail.com>
2014-05-31 12:05:11 +0300

    Tests for tape parsing.

--- unittests/test_input_parser.py
+++ unittests/test_input_parser.py
@@ -1,12 +1,18 @@
 import sys
 import unittest
 sys.path.append('/home/thepuppeteer/Projects/Python/Project/')
-from imput_parser import *
+from input_parser import *
 
 
 class InputParserTest(unittest.TestCase):
-    pass
+    def test_tape_parsing(self):
+        test_tape = parse_tape_from_terminal('(sdhfsdil,s,sadijsal)')
+        correct_tape = ["sdhfsdil",'s',"sadijsal"]
+        self.assertEqual(test_tape, correct_tape)
 
+        test_tape2 = parse_tape_from_terminal('   ( 01 10 101 0 10 ,5, 4   4 86 4)  ') 
+        correct_tape2 = [' 01 10 101 0 10 ','5',' 4   4 86 4']
+        self.assertEqual(test_tape2, correct_tape2)
 
 if __name__ == '__main__':
     unittest.main()
