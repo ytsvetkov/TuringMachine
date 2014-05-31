25a077fa3a88c9f6f6cdbbb5657bf1651a5fbc9a
Yosif Tsvetkov <yosif.tsvetkov.gmail.com>
2014-05-31 12:08:53 +0300

    Change because of Rule_Book construction.

--- unittests/test_rulebook.py
+++ unittests/test_rulebook.py
@@ -8,12 +8,12 @@ import rule_book
 class RulesTest(unittest.TestCase):
 
     def test_deterministic(self):
-        rule1 = rule.Rule(1, 'a', 'a', 1, 'Left')
-        rule2 = rule.Rule(1, 'a', 'b', 2, 'Right')
+        rule1 = [1, 'a', 'a', 1, 'Left']
+        rule2 = [1, 'a', 'b', 2, 'Right']
         self.assertRaises(TypeError, rule_book.Rule_Book, rule1, rule2)
 
-        rule3 = rule.Rule(1, 'b', 'a', 1, 'Right')
-        rules = rule_book.Rule_Book(rule1, rule3)
+        rule3 = [1, 'b', 'a', 1, 'Right']
+        rules = rule_book.Rule_Book([rule1, rule3])
         self.assertTrue(rules.is_deterministic())
 
 
