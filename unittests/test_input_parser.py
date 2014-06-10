9d6c75b91e694628958d672cf4f6143a7e660799
Yosif Tsvetkov <yosif.tsvetkov.gmail.com>
2014-06-11 01:34:53 +0300

    Some tests.

--- unittests/test_input_parser.py
+++ unittests/test_input_parser.py
@@ -5,14 +5,39 @@ from input_parser import *
 
 
 class InputParserTest(unittest.TestCase):
+
     def test_tape_parsing(self):
         test_tape = parse_tape_from_terminal('(sdhfsdil,s,sadijsal)')
-        correct_tape = ["sdhfsdil",'s',"sadijsal"]
+        correct_tape = ["sdhfsdil", 's', "sadijsal"]
         self.assertEqual(test_tape, correct_tape)
 
-        test_tape2 = parse_tape_from_terminal('   ( 01 10 101 0 10 ,5, 4   4 86 4)  ') 
-        correct_tape2 = [' 01 10 101 0 10 ','5',' 4   4 86 4']
+        test_tape2 = parse_tape_from_terminal(
+            '   ( 01 10 101 0 10 ,5, 4   4 86 4)  ')
+        correct_tape2 = [' 01 10 101 0 10 ', '5', ' 4   4 86 4']
         self.assertEqual(test_tape2, correct_tape2)
+
+    def tttttest_states_parsing(self):
+        test_states = {1,2,3}
+        parsed_states = parse_states_from_terminal('1, 2, 3')
+        self.assertEqual(test_states, parsed_states)
+
+    def test_initial_state_parse(self):
+        self.assertEqual(1, parse_initial_from_terminal('1'))
+        self.assertEqual(123, parse_initial_from_terminal('123'))
+        self.assertNotEqual(1212, parse_initial_from_terminal('1212  '))
+        self.assertNotEqual(1321, parse_initial_from_terminal('1212'))
+        self.assertNotEqual(1212, parse_initial_from_terminal('213'))
+        self.assertNotEqual(1212, parse_initial_from_terminal('1 21 2'))
+
+    def test_rules(self):
+        self.assertEqual([1,'a','a',1,'None'], parse_rule_from_terminal('(1,a,a,1,None)'))
+        self.assertEqual([1,'a','b',2,'Left'], parse_rule_from_terminal('(1,a,b,2,Left)'))
+        self.assertEqual([1,'a','a',1,'Right'], parse_rule_from_terminal('(1,a,a,1,Right)'))
+        self.assertNotEqual([2,'a','a',1,'Right'], parse_rule_from_terminal('(1,a,a,1,Right)'))
+        self.assertNotEqual([1,'b','a',1,'Right'], parse_rule_from_terminal('(1,a,a,1,Right)'))
+        self.assertNotEqual([1,'a','v',1,'Right'], parse_rule_from_terminal('(1,a,a,1,Right)'))
+        self.assertNotEqual([1,'a','a',3,'Right'], parse_rule_from_terminal('(1,a,a,1,Right)'))
+        self.assertNotEqual([1,'a','a',1,'Left'], parse_rule_from_terminal('(1,a,a,1,Right)'))
 
 if __name__ == '__main__':
     unittest.main()
--- unittests/test_rule.py
+++ unittests/test_rule.py
@@ -5,8 +5,32 @@ from rule import Rule
 
 
 class RuleTest(unittest.TestCase):
-    pass
+    
+    def test_can_be_applied(self):
+        rule = Rule(1,'a', 'b', 2, 'Left')
+        self.assertTrue(rule.can_be_applied(1,'a'))
+        self.assertFalse(rule.can_be_applied(2,'a'))
+        self.assertFalse(rule.can_be_applied(1,'b'))
 
+    def test_equality(self):
+        rule1 = Rule(1,'a', 'f', 2, 'None')
+        rule2 = Rule(1,'a', 'f', 2, 'None')
+        rule3 = Rule(1,'a', 'f', 3, 'None')
+        rule4 = Rule(2,'a', 'f', 3, 'None')
+        rule5 = Rule(2,'2', 'f', 3, 'None')
+        rule6 = Rule(2,'2', 'w', 3, 'None')
+        rule7 = Rule(2,'2', 'w', 3, 'Left')
+        rule8 = Rule(22,'A', 'C', 23, 'Right')
+
+        self.assertTrue(rule1 == rule1)
+        self.assertTrue(rule1 == rule2)
+        self.assertFalse(rule1 == rule3)
+        self.assertFalse(rule1 == rule4)
+        self.assertFalse(rule1 == rule5)
+        self.assertFalse(rule1 == rule6)
+        self.assertFalse(rule1 == rule7)
+        self.assertFalse(rule1 == rule8)
+        self.assertFalse(rule7 == rule1)
 
 if __name__ == '__main__':
     unittest.main()
