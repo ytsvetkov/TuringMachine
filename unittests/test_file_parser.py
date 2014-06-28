6025d74590cc903b031a091d1c15e66551010590
Yosif Tsvetkov <yosif.tsvetkov.gmail.com>
2014-06-28 14:28:20 +0300

    More tests.

--- unittests/test_file_parser.py
+++ unittests/test_file_parser.py
@@ -6,8 +6,100 @@ from parsing.file_parser import *
 
 
 class FileParserTest(unittest.TestCase):
-    pass
+    
+    def test_parse_tape(self):
+        self.assertEqual(parse_tape_from_file('tape: (asd,a,asd)', 0), ['asd', 'a', 'asd'])
+        self.assertEqual(parse_tape_from_file('tape: (,a,asd)', 0), ['', 'a', 'asd'])
+        self.assertEqual(parse_tape_from_file('tape: (,,)', 0), ['', '', ''])
+        self.assertEqual(parse_tape_from_file('tape: (asd,a,)', 0), ['asd', 'a', ''])
+        self.assertEqual(parse_tape_from_file('tape: (ada#dsd,a,afdas)d)', 0), ['ada#dsd', 'a', 'afdas)d'])
 
+        with self.assertRaises(SyntacticError):
+            self.assertEqual(parse_tape_from_file('tpe: (asd,a,asd)', 0), '')
+        with self.assertRaises(SyntacticError):
+            self.assertEqual(parse_tape_from_file('tape: (asda,asd)', 0), '')
+        with self.assertRaises(SyntacticError):
+            self.assertEqual(parse_tape_from_file('tape: (asdaasd)', 0), '')
+        with self.assertRaises(SyntacticError):
+            self.assertEqual(parse_tape_from_file('tape: (asd,a,asd', 0), '')
+        with self.assertRaises(SyntacticError):
+            self.assertEqual(parse_tape_from_file('tape: asd,a,asd)', 0), '')
+        with self.assertRaises(SyntacticError):
+            self.assertEqual(parse_tape_from_file('tape (asd,a,asd)', 0), '')
+        with self.assertRaises(SyntacticError):
+            self.assertEqual(parse_tape_from_file('(tape asd,a,asd)', 0), '')
+
+    def test_parse_states(self):
+        self.assertEqual(parse_states_from_file('states: {1,2,3,4,5}', 0),
+            set([1, 2, 3,4,5]))
+        self.assertEqual(parse_states_from_file('states: {1}', 0), set([1]))
+        self.assertEqual(parse_states_from_file('states: {}', 0), set())
+
+        with self.assertRaises(SyntacticError):
+            self.assertEqual(parse_states_from_file('stes: {}', 0), 0)
+        with self.assertRaises(SyntacticError):
+            self.assertEqual(parse_states_from_file('states {}', 0), 0)
+        with self.assertRaises(SyntacticError):
+            self.assertEqual(parse_states_from_file('states: {1,2,3', 0), 0)
+
+
+    def test_parse_initial(self):
+        self.assertEqual(parse_initial_from_file('initial: 0', 0), 0)
+        self.assertEqual(parse_initial_from_file('initial: 123', 0), 123)
+        self.assertEqual(parse_initial_from_file('initial: 10', 0), 10)
+        self.assertEqual(parse_initial_from_file('initial: 00', 0), 0)
+
+        with self.assertRaises(SyntacticError):
+            self.assertEqual(parse_initial_from_file('initial 0', 0), 0)
+        with self.assertRaises(SyntacticError):
+            self.assertEqual(parse_initial_from_file('inial: 0', 0), 0)
+        with self.assertRaises(SyntacticError):
+            self.assertEqual(parse_initial_from_file('inial:', 0), 0)
+
+    def test_parse_rule(self):
+        self.assertEqual(parse_rule_from_file('(0,b,a,b,0,Right)', 0),
+            [0, 'b', 'a', 'b', 0, 'Right'])
+        self.assertEqual(parse_rule_from_file('(0,_,_,_,1,None)', 0),
+            [0, '_', '_', '_', 1, 'None'])
+        self.assertEqual(parse_rule_from_file('(0,_,_,_,1,Left)', 0),
+            [0, '_', '_', '_', 1, 'Left'])
+
+        with self.assertRaises(SyntacticError):
+            self.assertEqual(parse_rule_from_file('(0,_,_,_,1,No)', 0), 0)
+        with self.assertRaises(SyntacticError):
+            self.assertEqual(parse_rule_from_file('(0,_,_,_,None)', 0), 0)
+        with self.assertRaises(SyntacticError):
+            self.assertEqual(parse_rule_from_file('(a,_,_,_,1,None)', 0), 0)
+        with self.assertRaises(SyntacticError):
+            self.assertEqual(parse_rule_from_file('(0,_,_,_,b,None)', 0), 0)
+        with self.assertRaises(SyntacticError):
+            self.assertEqual(parse_rule_from_file('(0,_,_,_,1,None', 0), 0)
+
+    def test_parse_accept_states(self):
+        self.assertEqual(parse_accept_states_from_file('accept: {1}', 0),
+            set([1]))
+        self.assertEqual(parse_accept_states_from_file('accept: {1}', 0), set([1]))
+        self.assertEqual(parse_accept_states_from_file('accept: {}', 0), set())
+
+        with self.assertRaises(SyntacticError):
+            self.assertEqual(parse_accept_states_from_file('acce: {}', 0), 0)
+        with self.assertRaises(SyntacticError):
+            self.assertEqual(parse_accept_states_from_file('accept {}', 0), 0)
+        with self.assertRaises(SyntacticError):
+            self.assertEqual(parse_accept_states_from_file('accept: {1,2,3', 0), 0)
+
+    def test_parse_reject_states(self):
+        self.assertEqual(parse_reject_states_from_file('reject: {1}', 0),
+            set([1]))
+        self.assertEqual(parse_reject_states_from_file('reject: {1}', 0), set([1]))
+        self.assertEqual(parse_reject_states_from_file('reject: {}', 0), set())
+
+        with self.assertRaises(SyntacticError):
+            self.assertEqual(parse_reject_states_from_file('acce: {}', 0), 0)
+        with self.assertRaises(SyntacticError):
+            self.assertEqual(parse_reject_states_from_file('reject {}', 0), 0)
+        with self.assertRaises(SyntacticError):
+            self.assertEqual(parse_reject_states_from_file('reject: {1,2,3', 0), 0)
 
 if __name__ == '__main__':
     unittest.main()
--- unittests/test_rulebook.py
+++ unittests/test_rulebook.py
@@ -2,7 +2,7 @@ import os
 import sys
 import unittest
 sys.path.append(os.getcwd())
-from models import rule_book
+from models import rule_book, rule
 
 
 
@@ -16,6 +16,34 @@ class RulesTest(unittest.TestCase):
         rule3 = [1, 'b', 'a', 'v',1, 'Right']
         rules = rule_book.Rule_Book([rule1, rule3])
         self.assertTrue(rules.is_deterministic())
+
+        rule5 = [1, 'a', 'д', 'v',1, 'None']
+        rule6 = [1, 'a', 'b', 'е',2, 'None']
+        self.assertRaises(TypeError, rule_book.Rule_Book, rule5, rule6)
+
+    def test_apply(self):
+        rule1 = [1, 'a', 'a', 'v',1, 'Left']
+        rule2 = [2, 'b', 'a', 'v',1, 'Right']
+        rule3 = [3, 'c', 'a', 'v',1, 'Left']
+        book = rule_book.Rule_Book([rule1, rule2, rule3])
+        self.assertTrue(book.can_be_applied(1, 'a'))
+        self.assertTrue(book.can_be_applied(2, 'b'))
+        self.assertTrue(book.can_be_applied(3, 'c'))
+        self.assertFalse(book.can_be_applied(21, 'a'))
+        self.assertFalse(book.can_be_applied(31, 'v'))
+        self.assertFalse(book.can_be_applied(3, 'l'))
+
+    def test_get_rule(self):
+        rule1 = [1, 'a', 'a', 'v',1, 'Left']
+        rule2 = [2, 'b', 'a', 'v',1, 'Right']
+        rule3 = [3, 'c', 'a', 'v',1, 'Left']
+        book = rule_book.Rule_Book([rule1, rule2, rule3])
+        self.assertEqual(book.get_rule(1,'a'), rule.Rule(*rule1))
+        self.assertEqual(book.get_rule(2,'b'), rule.Rule(*rule2))
+        self.assertEqual(book.get_rule(3,'c'), rule.Rule(*rule3))
+        self.assertEqual(book.get_rule(1,'b'), None)
+        self.assertEqual(book.get_rule(22,'v'), None)
+        self.assertEqual(book.get_rule(33,'c'), None)
 
 
 if __name__ == '__main__':
